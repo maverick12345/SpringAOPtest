31e6a4822cb77c8c873ea1ead7bb2efbe1690eff
vishnu <>
2017-02-16 20:16:55 +0530

    git test change

--- SpringAOP/src/org/aspect/LoggingAspect.java
+++ SpringAOP/src/org/aspect/LoggingAspect.java
@@ -8,7 +8,5 @@ public class LoggingAspect {
 	@Before("execution(public String org.shapes.Triangle.getName())")
 	public void aspectAdvice() {
 		System.out.println("aspect running.....");
-		System.out.println("aspect method is  running");
-		System.out.println("aspect method is  runningggg");
-	}
+		}
 }
